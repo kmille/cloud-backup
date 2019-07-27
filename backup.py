dc77a1ec39002d653b6353b979e1be1bbd9e5d23
kmille <christian.schneider@androidloves.me>
2019-07-27 10:34:29 +0200

    Improvements

--- backup.py
+++ backup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import configparser
 import subprocess
 import os.path
 import os
@@ -10,7 +11,10 @@ from ipdb import set_trace
 location_files_to_backup = "/home/kmille/tmp/files-to-backup"
 location_borg_files = "/home/kmille/tmp/borg-files"
 location_tmp = "/home/kmille/tmp/tmp"
+location_rclone_config = "/home/kmille/projects/cloud-backup/rclone.conf"
 prefix_backup_name = "cloud-backup"
+
+list_cloud_provider = []
 
 # BEGIN COMMANDS
 # BEGIN make backup
@@ -21,19 +25,17 @@ cmd_check_backup = "borg check --verbose {location_borg_files}"
 # BEGIN push backups in da cloud
 cmd_rclone_sync_to_cloud = "rclone sync {location_borg_files} {cloud_provider}: --progress"
 cmd_rclone_check = "rclone check {dir_to_check} {cloud_provider}:"
-cmd_rclone_size = "rclone size {cloud_provider}:"
+#cmd_rclone_size = "rclone size {cloud_provider}:"
 # END push backups in da cloud
 
 cmd_rclone_syn_to_disk = "rclone sync {cloud_provider}: {rclone_dir} --progress"
 #export RCLONE_CONFIG=/home/kmille/tmp/rclone/rclone.conf
 
-cmd_borg_list_last_backup_name = "borg list {rclone_dir} --json | jq '.archives[-1].name'"
+cmd_borg_list_last_backup_name = "borg list {rclone_dir} --json | jq -r '.archives[-1].name'"
 cmd_borg_mount_backup = "borg mount {rclone_dir}::{backup_name} {mount_dir}"
 
 cmd_borg_umount = "borg umount {mount_dir}"
 # END COMMANDS
-
-os.environ['BORG_RELOCATED_REPO_ACCESS_IS_OK'] = 'YES'
 
 
 def check_dir(dir):
@@ -42,15 +44,20 @@ def check_dir(dir):
         sys.exit(1)
 
 
-def check_directories():
+def init():
+    global list_cloud_provider
     check_dir(location_files_to_backup)
     check_dir(location_borg_files)
     check_dir(location_tmp)
+    os.environ['BORG_RELOCATED_REPO_ACCESS_IS_OK'] = 'YES'
+    os.environ['RCLONE_CONFIG'] = location_rclone_config
+    config = configparser.ConfigParser()
+    config.read(location_rclone_config)
+    list_cloud_provider = config.sections()
 
 
 def execute(cmd):
     cprint(f"Executing: '{cmd}'", 'magenta')
-    #return
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     p.wait()
     stdout, stderr = p.communicate()
@@ -67,24 +74,28 @@ def execute(cmd):
 
 
 def do_backup():
-    cprint("Let's make a backup", 'green')
+    cprint("Let's make a borg backup", 'green')
     execute(cmd_create_backup)
     execute(cmd_check_backup.format(location_borg_files=location_borg_files))
+    cprint("Done making a brog backup", 'green')
 
 def put_backup_in_da_cloud(cloud_provider):
-    cprint("Let's put the backup in da cloud", 'green')
+    cprint("Let's put the borg files in da cloud", 'green')
     execute(cmd_rclone_sync_to_cloud.format(location_borg_files=location_borg_files,
                                             cloud_provider=cloud_provider))
     execute(cmd_rclone_check.format(dir_to_check=location_borg_files,
                                     cloud_provider=cloud_provider))
-    execute(cmd_rclone_size.format(cloud_provider=cloud_provider))
+    #execute(cmd_rclone_size.format(cloud_provider=cloud_provider))
+    cprint("Done putting the borg files in da cloud", 'green')
 
 
 def download_backup(cloud_provider, rclone_dir):
+    cprint(f"Let's download the cloud backup of {cloud_provider} to {rclone_dir}", 'green')
     execute(cmd_rclone_syn_to_disk.format(cloud_provider=cloud_provider,
                                           rclone_dir=rclone_dir))
     execute(cmd_rclone_check.format(dir_to_check=rclone_dir,
                                     cloud_provider=cloud_provider))
+
 def test_backup(cloud_provider):
     cprint(f"Let's test the cloud backup of {cloud_provider}", 'green')
     rclone_dir = os.path.join(location_tmp, cloud_provider + "-files")
@@ -106,7 +117,7 @@ def mount_backup(cloud_provider, rclone_dir):
     execute(f"mkdir {mount_dir}")
     execute(cmd_check_backup.format(location_borg_files=rclone_dir))
     last_backup_name = execute(cmd_borg_list_last_backup_name.format(rclone_dir=rclone_dir))
-    print("this is the backup_name: {}".format(last_backup_name))
+    print(f"Last backup name: {last_backup_name}")
     execute(cmd_borg_mount_backup.format(rclone_dir=rclone_dir,
                                          backup_name=last_backup_name,
                                          mount_dir=mount_dir))
@@ -114,7 +125,9 @@ def mount_backup(cloud_provider, rclone_dir):
     execute(f"rm -rf {mount_dir}")
 
 
-check_directories()
-do_backup()
-put_backup_in_da_cloud("dropbox")
-test_backup("dropbox")
+if __name__ == '__main__':
+    init()
+    do_backup()
+    for cloud_provider in list_cloud_provider:
+        put_backup_in_da_cloud(cloud_provider)
+        test_backup(cloud_provider)
